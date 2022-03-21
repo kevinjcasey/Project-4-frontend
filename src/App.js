98c606490105b5f6e2bb3f87ed2186d05771f63d
Angel Valentin <angelgvalentin7@gmail.com>
2022-03-21 14:15:57 -0500

    Arranged flips buttons on carousel to match

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './components/Home'
-import { Add } from './components/Add'
-import { Edit } from './components/Edit'
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Add } from "./components/Add";
+import { Edit } from "./components/Edit";
 
 function App() {
-
-return (
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='add' element={<Add />}/>
-      <Route path='edit' element={<Edit />}/>
-    </Routes>
-  )
-  
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="add" element={<Add />} />
+            <Route path="edit" element={<Edit />} />
+        </Routes>
+    );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,22 +4,12 @@ import axios from "axios";
 
 // ============ MUI components ============ //
 
-
 import "../App.css";
-import { 
-  AppBar, 
-  Button, 
-  Card, 
-  CardActions, 
-  CardContent,
-  LinearProgress, 
-  Toolbar, 
-  Typography,
-} from "@mui/material"
+import { AppBar, Button, Card, CardActions, CardContent, LinearProgress, Toolbar, Typography } from "@mui/material";
 
-import PreviewIcon from '@mui/icons-material/Preview'
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
-import GitHubIcon from '@mui/icons-material/GitHub'
+import PreviewIcon from "@mui/icons-material/Preview";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 import { makeStyles } from "@mui/styles";
 
@@ -81,79 +71,77 @@ const useStyles = makeStyles((theme) => ({
         },
         marginRight: 16,
     },
+    CardAction: {
+        marginTop: "50px",
+    },
 }));
 // =========== ^^MUI Theme^^ ============= //
 
 export const Home = (props) => {
+    // ============= CRUD functions ============= //
 
-// ============= CRUD functions ============= //
+    const classes = useStyles();
 
-  const classes = useStyles();
+    const [flashcards, setFlashcards] = useState([]);
 
-  const [flashcards, setFlashcards] = useState([]);
-
-
-  const getFlashcard = () => {
-    axios
-         .get('http://localhost:8000/api/flashcards')
-         .then(
-           (response) => setFlashcards(response.data),
-           (err) => console.error(err)
-         )
-         .catch((error) => console.error(error))
-  }
-
+    const getFlashcard = () => {
+        axios
+            .get("http://localhost:8000/api/flashcards")
+            .then(
+                (response) => setFlashcards(response.data),
+                (err) => console.error(err)
+            )
+            .catch((error) => console.error(error));
+    };
 
     // =========== Progress Bar =========== //
 
+    const [progress, setProgress] = useState(0);
 
-  const [progress, setProgress] = useState(0)
+    const arrayOfNumbers = [1, 2, 3, 4, 5];
 
-  const arrayOfNumbers = [1,2,3,4,5]
-
-  const progressFunction = () => {
-  //   if (flashcards.question == flashcards[0]) {
-  //     setProgress(progress + 10)
-  //   } else if (flashcards.question == flashcards[1]) {
-      setProgress(progress + 10)
-  //   }
-  }
+    const progressFunction = () => {
+        //   if (flashcards.question == flashcards[0]) {
+        //     setProgress(progress + 10)
+        //   } else if (flashcards.question == flashcards[1]) {
+        setProgress(progress + 10);
+        //   }
+    };
 
     // Need to grab the index of the card displayed --
     // then show a number based on that index
     // if children.index = 2
     // display "in" progress bar '2 out of <children.length>'
 
-
     // Try messing around with props inside LinearProgress componenet?
 
-  // ============ Card Flip  =============== //
+    // ============ Card Flip  =============== //
 
-  const [isFlipped, setIsFlipped] = useState(false);
+    const [isFlipped, setIsFlipped] = useState(false);
 
-  const closeDetails = () => {
-      document.getElementById("details")
-      // .removeAttribute("open"); // This was throwing a warning
-  };
+    const closeDetails = () => {
+        document.getElementById("details");
+        // .removeAttribute("open"); // This was throwing a warning
+    };
 
-  const handleFlip= (e) => {
-      e.preventDefault();
-      setIsFlipped(!isFlipped);
-      closeDetails();
-  }
-  
-  // =========== UseEffect ========== //
+    const handleFlip = (e) => {
+        e.preventDefault();
+        setIsFlipped(!isFlipped);
+        closeDetails();
+    };
 
-  useEffect(() => {
-    getFlashcard()
-  }, [])
+    // =========== UseEffect ========== //
 
-  // ============ Browser =========== //
+    useEffect(() => {
+        getFlashcard();
+    }, []);
 
-  return (
-    <ThemeProvider theme={theme}>
-      {/* ----- Nav Bar ----- */}
-     <AppBar id="AppBar" position="relative" >
+    // ============ Browser =========== //
+
+    return (
+        <ThemeProvider theme={theme}>
+            {/* ----- Nav Bar ----- */}
+            <AppBar id="AppBar" position="relative">
                 <Toolbar>
                     <Typography style={{ textDecoration: "none" }} variant="h6">
                         <Link className={classes.AppBarLinks} to="/">
@@ -173,161 +161,116 @@ export const Home = (props) => {
                     </Typography>
                 </Toolbar>
             </AppBar>
-      {/* ----- App Name and Slogan ----- */}
-      <Typography 
-        variant="h2" 
-        align="center" 
-        color="textPrimary" 
-        
-      >
-      FlashPrep
-      </Typography>
-      <Typography 
-        variant="h5" 
-        align="center" 
-        color="textSecondary" 
-        paragraph
-      >
-      Index Cards On the Flash
-      </Typography>
-      {/* -------- Carousel ------- */}
-      <div className="flashcards">
-        <Typography 
-          variant="h6" 
-          color="primary" 
-          align="center"
-          >
-           <div id="carouselContainer">
-            <Carousel
-              className="carousel"
-              autoPlay={false}
-              indicators={true}
-              navButtonsAlwaysVisible={true}
-              animation="slide"
-              duration="400"
-              swipe="true"
-              // index={2}
-              onChange={progressFunction}
-              IndicatorIcon={arrayOfNumbers}
-              NavButton={({ onClick, className, style, next, prev }) => {
-                return (
-                  <Button 
-                    onClick={onClick}
-                    className={className} 
-                    style={style}>
-                    {next && "Next"}
-                    {prev && "Previous"}
-                  </Button>
-                );
-              }}
-            >
-              {flashcards.map((flashcard, index) => {
-                return (
-                  <div className="flashcard" key={flashcard.id}>
-                    <ReactCardFlip 
-                      isFlipped={isFlipped} 
-                      flipDirection="vertical" 
-                      spacing={4}
-                    >
-                      {/* --- Front of card --- */ }
-                      <div>
-                        <Card className="Card1">
-                          <Typography 
-                             padding="30px" 
-                            variant="h3"
-                          >
-                          Subject: {flashcard.subject}
-                          </Typography>
-                          <Typography 
-                          padding="50px" 
-                          variant="h4"
-                          >
-                          {flashcard.question}
-                          </Typography>
-                          <div className="CardAction">
-                            {/* ---- Flip button ---- */}
-                            <Button 
-                              variant="contained" 
-                              size="large" 
-                              color="primary" 
-                              onClick={handleFlip}
-                              startIcon={<PreviewIcon />}
-                            >
-                            Show Answer
-                            </Button>
-                          </div>
-                        </Card>
-                      </div>
-                      {/* --- Back of card --- */}
-                      <div>
-                        <Card className="Card1">
-                          <CardContent>
-                            <Typography 
-                               
-                              paddingTop="20px" 
-                              marginBottom="80px" 
-                              variant="h3"
-                            >
-                            </Typography>
-                            <Typography 
-                              variant="h4" 
-                              marginBottom="30px" 
-                              padding="50px"
-                            >
-                            {flashcard.answer}
-                            </Typography>
-                          </CardContent>
-                          <CardActions className="CardAction">
-                            <Button 
-                              variant="contained" 
-                              size="large" 
-                              color="primary" 
-                              onClick={handleFlip}
-                            >
-                            Flip
-                            </Button>
-                          </CardActions>
-                        </Card>
-                      </div>
-                    </ReactCardFlip>
-                  </div>
-                );
-              })}
-            </Carousel>
-           </div>
-                  <LinearProgress 
-                      sx={{ "paddingTop": "20px", "margin": "20px" }}
-                      variant="determinate"
-                      value={progress}
-                  />
-        </Typography>
-      </div>
-      <AppBar
-          id="AppBar"
-          position="fixed"
-          className='app'
-          sx={{ top: 'auto', bottom: -40 }}
-        >
-          <Toolbar className='footer'>
-            <div>
-              <a  className='link'href='https://www.linkedin.com/in/kris-garcia-3b7292146/'><LinkedInIcon/></a>
-              <a className='git' href='https://github.com/Weeechi'><GitHubIcon className='gitKris'/></a>
-              <h4>Kris Garcia</h4>
+            {/* ----- App Name and Slogan ----- */}
+            <Typography variant="h2" align="center" color="textPrimary">
+                FlashPrep
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                Index Cards On the Flash
+            </Typography>
+            {/* -------- Carousel ------- */}
+            <div className="flashcards">
+                <Typography variant="h6" color="primary" align="center">
+                    <div id="carouselContainer">
+                        <Carousel
+                            className="carousel"
+                            autoPlay={false}
+                            indicators={true}
+                            navButtonsAlwaysVisible={true}
+                            animation="slide"
+                            duration="400"
+                            swipe="true"
+                            // index={2}
+                            onChange={progressFunction}
+                            IndicatorIcon={arrayOfNumbers}
+                            NavButton={({ onClick, className, style, next, prev }) => {
+                                return (
+                                    <Button onClick={onClick} className={className} style={style}>
+                                        {next && "Next"}
+                                        {prev && "Previous"}
+                                    </Button>
+                                );
+                            }}
+                        >
+                            {flashcards.map((flashcard, index) => {
+                                return (
+                                    <div className="flashcard" key={flashcard.id}>
+                                        <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical" spacing={4}>
+                                            {/* --- Front of card --- */}
+                                            <div>
+                                                <Card className="Card1">
+                                                    <Typography marginBottom="20px" padding="30px" variant="h3">
+                                                        Subject: {flashcard.subject}
+                                                    </Typography>
+                                                    <Typography marginBottom="80px" padding="50px" variant="h4">
+                                                        {flashcard.question}
+                                                    </Typography>
+                                                    <div className="CardAction">
+                                                        {/* ---- Flip button ---- */}
+                                                        <Button variant="contained" size="large" color="primary" onClick={handleFlip} startIcon={<PreviewIcon />}>
+                                                            Show Answer
+                                                        </Button>
+                                                    </div>
+                                                </Card>
+                                            </div>
+                                            {/* --- Back of card --- */}
+                                            <div>
+                                                <Card className="Card1">
+                                                    <CardContent>
+                                                        <Typography paddingTop="20px" marginBottom="80px" variant="h3"></Typography>
+                                                        <Typography marginBottom="80px" variant="h4" padding="50px">
+                                                            {flashcard.answer}
+                                                        </Typography>
+                                                    </CardContent>
+                                                    <CardActions className="CardAction">
+                                                        <Button variant="contained" size="large" color="primary" paddingBottom="20px" onClick={handleFlip}>
+                                                            Flip
+                                                        </Button>
+                                                    </CardActions>
+                                                </Card>
+                                            </div>
+                                        </ReactCardFlip>
+                                    </div>
+                                );
+                            })}
+                        </Carousel>
+                    </div>
+                    <LinearProgress sx={{ paddingTop: "20px", margin: "20px" }} variant="determinate" value={progress} />
+                </Typography>
             </div>
-            <div className='div'>
-              <a  className='link'href='https://www.linkedin.com/in/kevin-j-casey/'><LinkedInIcon/></a>
-              <a  href='https://github.com/kevinjcasey'><GitHubIcon className='gitKev'/></a>
-              <h4 className='namekev'>Kevin J Casey</h4>
-            </div>
-            <div className='div'>
-              <a  className='link'href='https://www.linkedin.com/in/angelvalentin1/'><LinkedInIcon/></a>
-              <a className='gitAngel' href='https://github.com/angelgvalentin'><GitHubIcon className='gitAngel'/></a>
-            <h4 className='namekev'>Angel Valentin</h4>
-           </div>
-          </Toolbar>
-        </AppBar>
-    </ThemeProvider>
-  )
-}
+            <AppBar id="AppBar" position="fixed" className="app" sx={{ top: "auto", bottom: -40 }}>
+                <Toolbar className="footer">
+                    <div>
+                        <a className="link" href="https://www.linkedin.com/in/kris-garcia-3b7292146/">
+                            <LinkedInIcon />
+                        </a>
+                        <a className="git" href="https://github.com/Weeechi">
+                            <GitHubIcon className="gitKris" />
+                        </a>
+                        <h4>Kris Garcia</h4>
+                    </div>
+                    <div className="div">
+                        <a className="link" href="https://www.linkedin.com/in/kevin-j-casey/">
+                            <LinkedInIcon />
+                        </a>
+                        <a href="https://github.com/kevinjcasey">
+                            <GitHubIcon className="gitKev" />
+                        </a>
+                        <h4 className="namekev">Kevin J Casey</h4>
+                    </div>
+                    <div className="div">
+                        <a className="link" href="https://www.linkedin.com/in/angelvalentin1/">
+                            <LinkedInIcon />
+                        </a>
+                        <a className="gitAngel" href="https://github.com/angelgvalentin">
+                            <GitHubIcon className="gitAngel" />
+                        </a>
+                        <h4 className="namekev">Angel Valentin</h4>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </ThemeProvider>
+    );
+};
 
-export default Home
+export default Home;
